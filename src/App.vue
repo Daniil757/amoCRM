80df9639ccb861ed1cad7da9b4a4c06bfb2ce786
Daniil757 <perevozchikovdaniil1@gmail.com>
2023-10-17 23:33:03 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import DropDown from './components/DropDown.vue';
+import NewComponent from './components/NewComponent.vue';
+import './scss/main.scss'
+</script>
+
+<template>
+  <div>
+    <NewComponent/>
+    <DropDown/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/DropDown.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <select name="" id="" class="drop-down">
+        <option value="" class="drop-down__element drop-down__element_1">Не выбрано</option>
+        <option value="" class="drop-down__element drop-down__element_2">Сделка</option>
+        <option value="" class="drop-down__element drop-down__element_3">Контакт</option>
+        <option value="" class="drop-down__element drop-down__element_4">Компания</option>
+    </select>
+</template>
+
+<style scoped></style>
